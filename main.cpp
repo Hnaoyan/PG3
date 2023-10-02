ebb441be9fd3e5d171e93a4c679d0c4f616c898c
Hnaoyan <k022g00421@g.neec.ac.jp>
2023-10-02 11:21:49 +0900

    確認課題_01_01

--- main.cpp
+++ main.cpp
@@ -1,7 +1,44 @@
-#include <stdio.h>
+﻿#include <stdio.h>
+
+/// <summary>
+/// 比較して小さい値を返す関数
+/// </summary>
+/// <typeparam name="T"></typeparam>
+/// <param name="a"></param>
+/// <param name="b"></param>
+/// <returns></returns>
+template <typename T>
+T Min(T a, T b) {
+	if (a > b) {
+		return b;
+	}
+	else {
+		return a;
+	}
+}
+
+/// <summary>
+/// char型のオーバーライド
+/// </summary>
+/// <param name="a"></param>
+/// <param name="b"></param>
+/// <returns></returns>
+template <>
+char Min<char>(char a, char b) {
+	// 出力する文
+	printf("数字以外は代入できません");
+	// 空文字を返す
+	return '\0';
+}
 
 int main() {
-
-
+	// int型
+	printf("%d\n", Min<int>(10, 20));
+	// float型
+	printf("%f\n", Min<float>(10.0f, 20.0f));
+	// double型
+	printf("%lf\n", Min<double>(10.0, 20.0));
+	// char型
+	printf("%c\n", Min<char>('10', '20'));
 	return 0;
 }
