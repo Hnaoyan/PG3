24854a5d8153b3428971a491eef9e6acd2aea133
Hnaoyan <k022g00421@g.neec.ac.jp>
2023-10-02 12:52:30 +0900

    確認課題_01_02

--- main.cpp
+++ main.cpp
@@ -1,7 +1,45 @@
 #include <stdio.h>
 
-int main() {
+/// <summary>
+/// 再帰的な賃金計算
+/// </summary>
+/// <param name="money"></param>
+/// <returns></returns>
+int Recursive(int wage,int hour) {
+	int RemainingTime = hour - 1;
+	int recWage = wage * 2 - 50;
+	if (RemainingTime <= 1) {
+		return (recWage);
+	}
+	return (Recursive(recWage, RemainingTime));
+}
 
+/// <summary>
+/// 一般的な賃金計算
+/// </summary>
+/// <param name="wage"></param>
+/// <returns></returns>
+int HourlyWage(int wage,int hour) {
+	return wage * hour;
+}
+
+int main() {
+	// 一般的な賃金体系の時給
+	const int kPrevailingWage = 1072;
+	// 再帰的な賃金体系の初期値
+	int hourWage = 100;
+	// 働く時間
+	int timeValue = 9;
+	int result;
+	//printf("働く時間を入力してください...\n");
+	//scanf_s("%d\n", &timeValue);
+	// 再帰的な賃金計算の結果
+	result = Recursive(hourWage, timeValue);
+	printf("再帰的な賃金計算 : %d\n", result);
+
+	// 一般的な賃金計算の結果
+	result = HourlyWage(kPrevailingWage, timeValue);
+	printf("一般的な賃金計算 : %d\n", result);
 
 	return 0;
 }
