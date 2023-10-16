576d40197e94a87f17db591a168efd687fd1b40f
Hnaoyan <k022g0041@g.neec.ac.jp>
2023-10-16 23:31:04 +0900

    01_03_確認課題

--- main.cpp
+++ main.cpp
@@ -4,13 +4,40 @@
 
 typedef void (*PFunc)(int*);
 
+void Dice(int* answer) {
+
+	int dice = rand() % 6 + 1;
+
+	int surplus = dice % 2;
+	if (surplus == 0) {
+		printf("丁 : %d\n", dice);
+		if (*answer == 0) {
+			printf("あたり・・・！\n");
+		}
+		else {
+			printf("はずれ・・・！\n");
+		}
+	}
+	else if (surplus == 1) {
+		printf("半 : %d\n", dice);
+		if (*answer == 1) {
+			printf("あたり・・・！\n");
+		}
+		else {
+			printf("はずれ・・・！\n");
+		}
+	}
+}
+
 void SetTimeOut(PFunc p, int second) {
+	int answer = 0;
 	printf("丁：０ 半：１\n");
-	scanf("%d");
+	scanf_s("%d", &answer);
+	printf("結果は...\n");
 	// コールバックを呼び出す
 	Sleep(second * 1000);
 
-	p(&second);
+	p(&answer);
 }
 
 int main() {
@@ -18,6 +45,8 @@ int main() {
 	srand(time(nullptr));
 
 	PFunc p;
+	p = Dice;
+	SetTimeOut(p,3);
 
 	return 0;
 }
