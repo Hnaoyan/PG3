8d612f96cefe348039a5afc748dac5b9d2be1848
Hnaoyan <k022g0041@g.neec.ac.jp>
2023-10-16 12:04:55 +0900

    コールバックの途中

--- main.cpp
+++ main.cpp
@@ -1,7 +1,23 @@
 #include <stdio.h>
+#include <Windows.h>
+#include <time.h>
+
+typedef void (*PFunc)(int*);
+
+void SetTimeOut(PFunc p, int second) {
+	printf("丁：０ 半：１\n");
+	scanf("%d");
+	// コールバックを呼び出す
+	Sleep(second * 1000);
+
+	p(&second);
+}
 
 int main() {
+	// 乱数初期化
+	srand(time(nullptr));
 
+	PFunc p;
 
 	return 0;
 }
