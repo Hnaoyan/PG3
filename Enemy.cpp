faaf2d3caf21c36e80f2ce511c853f97ff0a758b
Hnaoyan <k022g0041@g.neec.ac.jp>
2023-10-23 11:12:08 +0900

    関数ポインタテーブルの途中

---
+++ Enemy.cpp
@@ -0,0 +1,35 @@
+﻿#include "Enemy.h"
+#include <stdio.h>
+
+Enemy::Enemy()
+{
+}
+
+void Enemy::Update()
+{
+	// 関数ポインタテーブル
+	(this->*pFuncTable[static_cast<size_t>(phase_)])();
+
+}
+
+void Enemy::Approach()
+{
+	printf("近接\n");
+}
+
+void Enemy::Shooting()
+{
+	printf("射撃\n");
+}
+
+void Enemy::BreakAway()
+{
+	printf("離脱\n");
+}
+
+void (Enemy::*Enemy::pFuncTable[])() 
+= {
+	&Enemy::Approach,
+	&Enemy::Shooting,
+	&Enemy::BreakAway
+};
---
+++ Enemy.h
@@ -0,0 +1,49 @@
+﻿#pragma once
+class Enemy
+{
+public:	// サブクラス
+	enum class State {
+		kApproach,
+		kShooting,
+		kBreakAway,
+	};
+
+public: // メンバ関数
+	/// <summary>
+	/// コンストラクタ
+	/// </summary>
+	Enemy();
+	/// <summary>
+	/// デストラクタ
+	/// </summary>
+	~Enemy() = default;
+
+	/// <summary>
+	/// 更新処理
+	/// </summary>
+	void Update();
+
+private:
+	/// <summary>
+	/// 近接関数
+	/// </summary>
+	void Approach();
+
+	/// <summary>
+	/// 射撃関数
+	/// </summary>
+	void Shooting();
+
+	/// <summary>
+	/// 離脱関数
+	/// </summary>
+	void BreakAway();
+
+private:
+	// メンバ関数のテーブル
+	static void(Enemy::*pFuncTable[])();
+
+	// 状態管理変数（EnumClass）
+	State phase_ = State::kApproach;
+};
+
