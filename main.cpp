e3e400ff42b53609ce5e9ea172be48b51fc9bc91
Hnaoyan <k022g0041@g.neec.ac.jp>
2023-11-06 12:58:09 +0900

    クラス作成

--- main.cpp
+++ main.cpp
@@ -1,8 +1,22 @@
 #include <stdio.h>
+#include "Circle.h"
+#include "Rectangle.h"
+#include "IShape.h"
+#include <memory>
 
 int main() {
-	char str[] = "テスト";
-	printf("%s", str);
+	// 円クラス
+	std::unique_ptr<Circle> circle_ = std::make_unique<Circle>();
+	// 矩形クラス
+	std::unique_ptr<Rectangle> rectangle_ = std::make_unique<Rectangle>();
+
+	// 計算
+	circle_->Size();
+	rectangle_->Size();
+
+	// 出力
+	circle_->Draw();
+	rectangle_->Draw();
 
 	return 0;
 }
