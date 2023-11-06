c1e1bf5a416c81a7d3412afbe5b495b6e73bd16d
Hnaoyan <k022g0041@g.neec.ac.jp>
2023-11-06 13:00:41 +0900

    02_04_確認課題

---
+++ Circle.cpp
@@ -0,0 +1,18 @@
+﻿#include "Circle.h"
+
+void Circle::Size()
+{
+	// 初期設定
+	radius_ = 5.0f;
+	float pi = 3.14f;
+
+	// 面積
+	area_ = radius_ * radius_ * pi;
+}
+
+void Circle::Draw()
+{
+	// 出力結果
+	std::cout << "円の面積：" << area_ << std::endl;
+
+}
--- IShape.h
+++ IShape.h
@@ -1,5 +1,21 @@
-#pragma once
+﻿#pragma once
+#include <iostream>
+#include <cmath>
+
 class IShape
 {
-};
+public: // メンバ関数
+	virtual void Size() = 0;
+	virtual void Draw() = 0;
 
+protected:
+	// 面積
+	float area_;
+
+	// 半径
+	float radius_;
+	// 幅
+	float width_;
+	// 高さ
+	float height_;
+};
