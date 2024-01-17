4f6d28f26e1eca521e9dd053840e0e8fabe8bd1e
Hnaoyan <k022g0041@g.neec.ac.jp>
2024-01-17 13:01:24 +0900

    06_02_確認課題

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
-#include <stdio.h>
+#include <iostream>
+#include <thread>
+
+void PrintScreen(int num) {
+	std::cout << "thread " << num << std::endl;
+}
 
 int main() {
 
+	std::thread th1(PrintScreen, 1);
+	th1.join();
+
+	std::thread th2(PrintScreen, 2);
+	th2.join();
+
+	std::thread th3(PrintScreen, 3);
+	th3.join();
 
 	return 0;
 }
