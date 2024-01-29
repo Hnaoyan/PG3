e9fb74d44cd5317cac1c371c9e6c2144876af8ae
Hnaoyan <k022g0041@g.neec.ac.jp>
2024-01-29 10:34:21 +0900

    初期化まで

--- main.cpp
+++ main.cpp
@@ -1,20 +1,17 @@
 #include <iostream>
 #include <thread>
-
-void PrintScreen(int num) {
-	std::cout << "thread " << num << std::endl;
-}
+#include <queue>
+#include <condition_variable>
+#include <mutex>
 
 int main() {
 
-	std::thread th1(PrintScreen, 1);
-	th1.join();
+	std::mutex mutex;
+	std::condition_variable condition;
+	std::queue<int> q;
+	bool exit = false;
 
-	std::thread th2(PrintScreen, 2);
-	th2.join();
 
-	std::thread th3(PrintScreen, 3);
-	th3.join();
 
 	return 0;
 }
