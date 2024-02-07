6e99116f45a604ad2b13ad1e241c9b06db9a65f1
Hnaoyan <k022g0041@g.neec.ac.jp>
2024-02-07 20:52:37 +0900

    確認課題

--- main.cpp
+++ main.cpp
@@ -1,16 +1,35 @@
 #include <iostream>
-#include <thread>
-#include <queue>
-#include <condition_variable>
-#include <mutex>
+#include <string>
+#include <chrono>
 
-int main() {
+int main() 
+{
+	// タイマー関係
+	std::chrono::system_clock::time_point start, end;
+	// コピー
+	start = std::chrono::system_clock::now();
 
-	std::mutex mutex;
-	std::condition_variable condition;
-	std::queue<int> q;
-	bool exit = false;
+	std::string a = (100000, "a");
+	std::string x = a;
 
+	end = std::chrono::system_clock::now();
+
+	std::chrono::microseconds time1 = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+
+	// 移動
+	start = std::chrono::system_clock::now();
+
+	std::string y = std::move(a);
+
+	end = std::chrono::system_clock::now();
+
+	std::chrono::microseconds time2 = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+
+	std::cout << "100000文字の移動とコピー比較" << std::endl;
+
+	std::cout << "コピー: " << static_cast<int>(time1.count()) << std::endl;
+
+	std::cout << "移動 " << static_cast<int>(time2.count()) << std::endl;
 
 
 	return 0;
